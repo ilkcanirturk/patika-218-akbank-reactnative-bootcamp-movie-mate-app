de8b3565d6c6fa8e70a3dd8a74863d100a39f7a3
ilkcan irturk <ilkcan.irturk@outlook.com>
2022-09-09 18:58:36 +0300

    edit changes

--- MovieApp/src/screens/SearchScreen.js
+++ MovieApp/src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {StyleSheet, TextInput, View} from 'react-native';
 import React, {useContext} from 'react';
 
 import {ThemeContext} from '../context/theme';
