2f85132a7b78449be79c5a3a070db6312f0996f4
ilkcan irturk <ilkcan.irturk@outlook.com>
2022-09-09 18:50:41 +0300

    add Movie Details Screen

---
+++ MovieApp/src/screens/MovieDetailsScreen.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+
+
+//Needs to be develop.
+const MovieDetailsScreen = () => {
+  return (
+    <View>
+      <Text>MovieDetailsScreen</Text>
+    </View>
+  )
+}
+
+export default MovieDetailsScreen
+
+const styles = StyleSheet.create({})
